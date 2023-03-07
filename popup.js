eca4979dc40ae646032fdc14aefe0c01fb9b3e21
Andrew Niziolek <aniziolek9@gmail.com>
2023-03-06 23:26:24 -0500

    JS Var References, Misc
    Added JS var references for more checkboxes, adjusted spacing of lines.

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 const fbtn = document.getElementById("fillbutton")
 
 const aid = document.getElementById("affID")
+const sourCh = document.getElementById("sourCh")
+const sourMe = document.getElementById("sourMe")
+const camp = document.getElementById("camp")
 
 fbtn.addEventListener('click', function(){
 
