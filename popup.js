770fe40113687a95687befa2dc67454868651fee
Andrew Niziolek <aniziolek9@gmail.com>
2023-03-06 23:51:17 -0500

    Logic Updates
    Added more if controls, began function for output combination.

--- popup.js
+++ popup.js
@@ -1,15 +1,38 @@
-const fbtn = document.getElementById("fillbutton")
+const fbtn = document.getElementById("fillbutton");
+const outField = document.getElementById("outputField");
+var ampCounter = 0; /* Counts the number of values checked */
 
-const aid = document.getElementById("affID")
-const sourCh = document.getElementById("sourCh")
-const sourMe = document.getElementById("sourMe")
-const camp = document.getElementById("camp")
+const aid = document.getElementById("affID");
+const sourCh = document.getElementById("sourCh");
+const sourMe = document.getElementById("sourMe");
+const camp = document.getElementById("camp");
+
+var aidout, sourChOut, sourMeOut, campOut;
+aidOut = sourChOut = sourMeOut = campOut = "";
 
 fbtn.addEventListener('click', function(){
 
     if(aid.checked == true){
-        alert("true")
-    } else {
-        alert ("false")
+        aidOut = "!!!affid!!!"
+        ampCounter ++
     }
+    if(sourCh.checked == true){
+        sourChOut = "sourChChecked"
+        ampCounter ++
+    }
+    if(sourMe.checked == true){
+        sourMeOut = "sourMeChecked"
+        ampCounter ++
+    }
+    /* Add rest of checks here */
+    else{
+        alert("Nothing is checked")
+
+    }
+
+    function combineOutput(){
+        
+    }
+
+    outField.value = aidOut + "&" + sourChOut
 });
