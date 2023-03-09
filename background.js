10934f41ad9f176b3b2596961c94d726be74e910
Andrew Niziolek <aniziolek9@gmail.com>
2023-03-09 11:53:50 -0500

    Header Styling
    Added version number to header

--- background.js
+++ background.js
@@ -1,10 +1,1 @@
 // All background functions that need to run throughout the extension.
-
-const MIDInput = document.getElementById("clipMID");
-var MIDval = MIDInput.value;
-
-await.chrome.storage.local.set({'MIDStore' : jsVariables}, function(){
-    if(chrome.runtime.error){
-        console.log("Error");
-    }
-})
