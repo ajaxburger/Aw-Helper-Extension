15e9aacb63430a3e239ae9d612bde276073ccf66
Andrew Niziolek <aniziolek9@gmail.com>
2023-03-08 19:24:19 -0500

    Added Nav buttons to common links

---
+++ scripts/navButton.js
@@ -0,0 +1,21 @@
+const UIbtn = document.getElementById("UINav");
+var UIURL = "https://ui.awin.com/user";
+
+const ProvArbtn = document.getElementById("PANav");
+var PAURL = "https://ui.awin.com/provider";
+
+const OProvArbtn = document.getElementById("OPANav");
+var OPAURL = "https://ui.awin.com/user/redirect/v1-provider/r/aHR0cHM6Ly91aTIuYXdpbi5jb20vYWRtaW5hcmVhL3Byb3ZpZGVyLw==";
+
+UIbtn.addEventListener('click', function(){
+    chrome.tabs.create({ url : UIURL })
+});
+
+ProvArbtn.addEventListener('click', function(){
+    chrome.tabs.create({ url : PAURL })
+});
+
+OProvArbtn.addEventListener('click', function(){
+    chrome.tabs.create({ url : OPAURL })
+});
+    
