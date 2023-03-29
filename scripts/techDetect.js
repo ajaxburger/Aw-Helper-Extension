606f7b40af398f55a4f78bc3068a51fbabd7bca7
Andrew Niziolek <aniziolek9@gmail.com>
2023-03-29 13:12:55 -0400

    Functioning GTM Detection

--- scripts/techDetect.js
+++ scripts/techDetect.js
@@ -1,22 +1,26 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-  let url = tabs[0].url;
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  const currentTab = tabs[0];
+  const url = currentTab.url;
 
-  if(!url.includes("chrome://") || !url.includes("awin.com")) {
-
+  if (!url.includes("chrome://") && !url.includes("awin.com")) {
     chrome.scripting.executeScript({
-      target: {tabId: tabs.id },
-      func: function() {
-        var hasGTM = document.documentElement.innerHTML.indexOf("gtm.js") !== -1;
-
-        statText.textContent = "GTM Detected.";
+      target: {tabId: currentTab.id},
+      function() {
+        const scripts = document.getElementsByTagName("script");
+        for (let i = 0; i < scripts.length; i++) {
+          if (scripts[i].src.includes("gtm.js")) {
+            return true;
+          }
+        }
+        return false;
       }
+    }, function(result) {
+      const containsGtm = result[0].result;
+      const gtmStatus = document.getElementById("GTMstatus");
+      gtmStatus.textContent = containsGtm ? "This page contains gtm.js" : "This page does not contain gtm.js";
     });
-
+  } else {
+    const gtmStatus = document.getElementById("GTMstatus");
+    gtmStatus.textContent = "Restricted URL.";
   }
-  else {
-    const statText = document.getElementById("GTMstatus");
-
-    statText.textContent = "Restricted URL.";
-  }
-
 });
