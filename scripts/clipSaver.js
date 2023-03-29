737d8b33c2e25bfb21a3ec325da53ab4685542f3
Andrew Niziolek <aniziolek9@gmail.com>
2023-03-29 12:18:20 -0400

    New Implementation + Significant Refactor
    New Implementation Feature is completed and will open all required new tabs to complete an implementation.
    
    JS files have been refactored to optimize code execution and simplify.
    
    Removed </input> tag closers as they are not required.

--- scripts/clipSaver.js
+++ scripts/clipSaver.js
@@ -3,84 +3,34 @@ const MIDBox = document.getElementById("clipMID");
 const CRBox = document.getElementById("clipCR");
 const NFBox = document.getElementById("clipNF");
 
-// Get value from storage, catch if undefined and ignore error.
-chrome.storage.local.get("MIDSave", function(MIDRecall) {
-
-    if(typeof MIDRecall == 'undefined'){
-        return;
-    }
-    else {
-        MIDBox.value = MIDRecall.MIDSave;
-    }
-
-});
-
-chrome.storage.local.get("CRSave", function(CRRecall) {
-
-    if(typeof CRRecall == 'undefined'){
-        return;
-    }
-    else {
-        CRBox.value = CRRecall.CRSave;
-    }
-
-});
-
-chrome.storage.local.get("NFSave", function(NFRecall) {
-
-    if(typeof NFRecall == 'undefined'){
-        return;
-    }
-    else {
-        NFBox.value = NFRecall.NFSave;
-    }
-
-});
-
-// Auto saving input as they're typed.
-
-MIDBox.addEventListener("input", function () {
-    var MIDBoxData = MIDBox.value;
-
-    chrome.storage.local.set({'MIDSave': MIDBoxData}, function(){
+// recallAndSet Function for use with Chrome Storage API
+function recallAndSet(box, key) {
+    chrome.storage.local.get(key, function(data) {
+      if (data[key]) {
+        box.value = data[key];
+      }
     });
+  
+    box.addEventListener("input", function () {
+      chrome.storage.local.set({[key]: box.value});
+    });
+  }
 
+// Calling rAS function with desired values
+  recallAndSet(MIDBox, "MIDSave");
+  recallAndSet(CRBox, "CRSave");
+  recallAndSet(NFBox, "NFSave");
+
+// Clear button
+const clearBtn = document.getElementById("clearClipBtn");
+clearBtn.addEventListener("click", function () {
+  MIDBox.value = "";
+  CRBox.value = "";
+  NFBox.value = "";
+
+  chrome.storage.local.set({
+    MIDSave: "",
+    CRSave: "",
+    NFSave: ""
+  });
 });
-
-CRBox.addEventListener("input", function(){
-    var CRBoxData = CRBox.value;
-
-    chrome.storage.local.set({'CRSave': CRBoxData}, function(){
-    });
-});
-
-NFBox.addEventListener("input", function () {
-    var NFBoxData = NFBox.value;
-
-    chrome.storage.local.set({'NFSave': NFBoxData}, function(){
-    });
-});
-
-// Clear button auto-saves data.
-
-const clear = document.getElementById("clearClipBtn").onclick = function (){
-    
-    MIDBox.value = ""; CRBox.value = ""; NFBox.value = "";
-
-    var MIDBoxData = MIDBox.value;
-    var CRBoxData = CRBox.value;
-    var NFBoxData = NFBox.value;
-
-    chrome.storage.local.set({'MIDSave': MIDBoxData}, function(){
-    });
-
-    chrome.storage.local.set({'CRSave': CRBoxData}, function(){
-    });
-
-    chrome.storage.local.set({'NFSave': NFBoxData}, function(){
-    });
-
-
-}
-
-
--- scripts/newImpl.js
+++ scripts/newImpl.js
@@ -1,11 +1,25 @@
-const implMIDBox = document.getElementById("implMID");
+// Capture HTML Elements
+const importMIDBox = document.getElementById("implMIDBox");
+const goBtn = document.getElementById("newImplBtn");
 
-const goBtn = document.getElementById("newImplBtn").onclick = function () {
-
-    var MIDValue = implMIDBox.value;
-
-    var URL1 = "https://ui.awin.com/tracking-settings/us/awin/advertiser/" + MIDValue.value + "/main-settings"
-
-    chrome.tabs.create({ url : URL1 })
-
+// createTabs function using the Chrome Tabs API
+function createTabs(URLs) {
+  URLs.forEach((url) => {
+    chrome.tabs.create({ url });
+  });
 }
+
+// goBtn activates the cT function.
+goBtn.addEventListener("click", () => {
+  const MIDValue = importMIDBox.value;
+  const URLs = [
+    `https://ui.awin.com/tracking-settings/us/awin/advertiser/${MIDValue}/main-settings`,
+    `https://ui.awin.com/commission-manager/us/awin/merchant/${MIDValue}/commission-groups`,
+    `https://ui.awin.com/advertiser-mastertag/us/awin/${MIDValue}/trackingtagsettings`,
+    `https://ui.awin.com/provider/merchant-settings/${MIDValue}/account-details/network/awin`,
+    `https://ui.awin.com/provider/finance/fee-manager/en/${MIDValue}`,
+    `https://ui.awin.com/provider/merchant-settings/${MIDValue}/mobile-tracking/network/awin`
+  ];
+
+  createTabs(URLs);
+});
