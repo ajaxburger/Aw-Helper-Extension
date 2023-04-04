72b2856a27806277a928733d9a8b0b0695e94837
Andrew Niziolek <aniziolek9@gmail.com>
2023-04-04 09:13:25 -0400

    Stylization / Other Small Updates

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 // All background functions that need to run throughout the extension.
 
 
-// Runs Clipboard save / recall on popup click
+// Runs Clipboard save and techDetect on popup click.
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.scripting.executeScript(null, {file: "/scripts/clipSaver"});
     chrome.scripting.executeScript(null, {file: "/scripts/techDetect.js"});
 
     console.log ("On-click function has fired");
- });
+});
