ab8dd15faf11d63b0345927617b89fc1e686093e
Andrew Niziolek <aniziolek9@gmail.com>
2024-07-08 22:24:11 -0400

    Integration Compatibility + Tech Progress
    The compatibility dialog has been visually added and scripted to show when in simple mode but hidden in tech mode. It is not yet functional on detections.
    
    Separate tech platform dialogs have been created for a number of compatible platforms and progress on their setup but no function as of yet.

--- scripts/modeSelect.js
+++ scripts/modeSelect.js
@@ -1,2 +1,38 @@
-// Capture HTML Element
-const importModeSwitch = document.getElementById("modeSwitch");
+document.addEventListener('DOMContentLoaded', function() {
+    const modeSwitch = document.getElementById('modeSwitch');
+    const newImplView = document.getElementById('newImplView');
+    const testURLTech = document.getElementById('testURLTech');
+    const clipboardView = document.getElementById('clipboard');
+    const compatibleView = document.getElementById('compatibleBox');
+
+    // Function to update the visibilty of page elements.
+    function updateButtonVisibility() {
+        if (modeSwitch.checked) {
+            // If tech mode enabled, display (block) or hide (none) these elements.
+            newImplView.style.display = 'block';
+            testURLTech.style.display = 'block';
+            clipboardView.style.display = 'block';
+            compatibleView.style.display = 'none';
+        } else {
+            newImplView.style.display = 'none';
+            testURLTech.style.display = 'none';
+            clipboardView.style.display = 'none';
+            compatibleView.style.display = 'block';
+        }
+    }
+
+    // Restore the state of the checkbox and update element visibility
+    chrome.storage.local.get(['modeSwitch'], function(result) {
+        if (result.modeSwitch !== undefined) {
+            modeSwitch.checked = result.modeSwitch;
+            updateButtonVisibility();
+        }
+    });
+
+    // Save the state of the checkbox and update element visibility when it is changed
+    modeSwitch.addEventListener('change', function() {
+        chrome.storage.local.set({modeSwitch: modeSwitch.checked}, function() {
+            updateButtonVisibility();
+        });
+    });
+});
