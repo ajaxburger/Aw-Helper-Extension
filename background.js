f1c17d1000514bc07fbe7f0e65711e447ab1bbde
Andrew Niziolek <aniziolek9@gmail.com>
2024-07-10 08:36:20 -0400

    Analytics Implementation
    Google Analytics is now implemented and returns data on the features used and only the features used.
    
    This will allow tracking of the most used and least used features for future development.

--- background.js
+++ background.js
@@ -1,8 +1,29 @@
-// All background functions that need to run throughout the extension.
+// Import the analytics script
+importScripts('./scripts/analytics.js');
+
+// Function to report element interactions
+async function reportElementInteraction(elementId) {
+  await analytics.fireEvent('element_interaction', { element_id: elementId });
+}
+
+// Event listener for unhandled rejections
+addEventListener('unhandledrejection', async (event) => {
+  analytics.fireErrorEvent(event.reason);
+});
+
+// Event listener for when the extension is installed
+chrome.runtime.onInstalled.addListener(() => {
+  analytics.fireEvent('install');
+});
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.techCount !== undefined) {
-      chrome.action.setBadgeText({text: request.techCount.toString(), tabId: sender.tab.id});
-      chrome.action.setBadgeBackgroundColor({color: "#4CAF50", tabId: sender.tab.id});
-    }
-  });
+  if (request.techCount !== undefined) {
+    chrome.action.setBadgeText({ text: request.techCount.toString(), tabId: sender.tab.id });
+    chrome.action.setBadgeBackgroundColor({ color: "#4CAF50", tabId: sender.tab.id });
+  }
+  if (request.elementId) {
+    console.log('Element interaction received:', request.elementId); // Log the received element ID
+    reportElementInteraction(request.elementId);
+  }
+});
+
