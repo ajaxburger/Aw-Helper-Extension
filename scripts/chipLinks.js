fa011e5e06bd960710fffbaac8ddb463ad4ab4a6
Andrew Niziolek <aniziolek9@gmail.com>
2024-07-10 21:06:50 -0400

    Analytics Fixes, Chip Linking, Code Cleanup
    Analytics was reading event interaction with the actual page rather than just the extension. Renamed content-scripts.js to popup.js and moved it to only load with in the head of the popup.
    
    Chips now link to their relevant documentation; need to figure out a better way to link to all documents.
    
    Removed excess code from un-adopted features. These may be added back in the future but will remain off of the build for now.

--- scripts/chipLinks.js
+++ scripts/chipLinks.js
@@ -1,1 +1,20 @@
-// Future Linking to Wiki articles from detection Chips.
+//Linking to docs from detection Chips.
+const GTMChip = document.getElementById("gtmChip");
+GTMChip.addEventListener("click", () => {
+    chrome.tabs.create({ url:"https://wiki.awin.com/index.php/GTM_UnconditionalFiring", active: false });
+});
+
+const ShopChip = document.getElementById("shopifyChip");
+ShopChip.addEventListener("click", () => {
+    chrome.tabs.create({ url:"https://wiki.awin.com/index.php/Awin_Access_Shopify_Tracking_Installation_Guide", active: false });
+});
+
+const wooChip = document.getElementById("wooCommChip");
+wooChip.addEventListener("click", () => {
+    chrome.tabs.create({ url:"https://wiki.awin.com/index.php/Awin_Access_WooCommerce_Tracking_Installation_Guide", active: false });
+});
+
+const magChip = document.getElementById("magentoChip");
+magChip.addEventListener("click", () => {
+    chrome.tabs.create({ url:"https://advertiser-success.awin.com/s/article/Integrating-Magento-2-4", active: false });
+});
